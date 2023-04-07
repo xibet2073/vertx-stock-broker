e92b230724f8c1e1bc2bdcfb45a65f99179e1a61
xibet2073 <fosol2073@gmail.com>
2023-04-07 14:12:10 +0200

    WebClient

--- src/main/java/com/keelient/vertx_stock_broker/MainVerticle.java
+++ src/main/java/com/keelient/vertx_stock_broker/MainVerticle.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
   public static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+  public static final int PORT = 8888;
+
   public static void main(String[] args) {
     var vertx = Vertx.vertx();
     vertx.exceptionHandler(error -> {
@@ -34,7 +36,7 @@ public class MainVerticle extends AbstractVerticle {
     vertx.createHttpServer()
       .requestHandler(restApi)
       .exceptionHandler(error -> LOG.error("HTTP Server error: ", error))
-      .listen(8888, http -> {
+      .listen(PORT, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         LOG.info("HTTP server started on port 8888");
---
+++ src/test/java/com/keelient/vertx_stock_broker/TestAssetRestApi.java
@@ -0,0 +1,43 @@
+package com.keelient.vertx_stock_broker;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(VertxExtension.class)
+public class TestAssetRestApi {
+  public static final Logger LOG = LoggerFactory.getLogger(TestAssetRestApi.class);
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void returnsAllAssets(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    WebClient webClient = WebClient.create(
+            vertx,
+            new WebClientOptions()
+                    .setDefaultPort(MainVerticle.PORT)
+    );
+
+    webClient.get("/assets")
+            .send()
+            .onComplete(testContext.succeeding(response -> {
+              JsonArray json = response.bodyAsJsonArray();
+              LOG.info("Response: {}", json);
+              assertEquals("[{\"name\":\"AAPL\"},{\"name\":\"AMZN\"},{\"name\":\"TSLA\"}]", json.encode());
+              assertEquals(200, response.statusCode());
+              testContext.completeNow();
+            }));
+  }
+}
